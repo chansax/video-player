13de711a6930418c5dd6d66cae177dc2c368aaa6
Chandan Saxena <chansax@gmail.com>
2019-09-05 08:38:30 -0400

    test cases

---
+++ app/src/androidTest/java/com/chansax/videoplayer/adapter/VideoAdapterTest.kt
@@ -0,0 +1,30 @@
+package com.chansax.videoplayer.adapter
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by chandan on 2019-09-05.
+ */
+
+class VideoAdapterTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getItemCount() {
+    }
+
+    @Test
+    fun setItems() {
+    }
+}
---
+++ app/src/test/java/com/chansax/videoplayer/data/VideoInfoTest.kt
@@ -0,0 +1,30 @@
+package com.chansax.videoplayer.data
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by chandan on 2019-09-05.
+ */
+
+class VideoInfoTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getTitle() {
+    }
+
+    @Test
+    fun getUrl() {
+    }
+}
